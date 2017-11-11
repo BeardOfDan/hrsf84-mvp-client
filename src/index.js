ba7f932bea8777b2607afc87df4f29d732d146df
Daniel Roberts <BeardOfDan@gmail.com>
2017-11-11 14:16:51 -0800

    Add a precurser to the navbar

--- src/index.js
+++ src/index.js
@@ -10,14 +10,19 @@ import {
 import Story from './Story.jsx';
 import Login from './Login.jsx';
 import Signup from './Signup.jsx';
+import NavBar from './NavBar.jsx';
 
 const BasicExample = () => (
   <Router>
     <div>
+      {<Route path='*' component={NavBar} />}
+
       {<Route path='/login' component={Login} />}
       {<Route path='/signup' component={Signup} />}
 
       {/* TODO: Figure out how to only have the below route execute if the above routes are not a match */}
+
+      {/* Possibly an if statement, for the path vs the options above */}
 
       {<Route path='*' component={Story} />}
     </div>
