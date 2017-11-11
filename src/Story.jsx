b9c2400e23e5fccf54efb917318ff4b2d4794df1
Daniel Roberts <BeardOfDan@gmail.com>
2017-11-11 11:24:25 -0800

    Refactor some of the code

--- src/Story.jsx
+++ src/Story.jsx
@@ -10,65 +10,22 @@ export default class Story extends React.Component {
     const path = this.props.location.pathname;
     const fullPath = `https://hrsf84-mvp-server.herokuapp.com${path}`;
 
-    this.state = {};// json format of the story
+    this.state = {}; // will recieve a json format of the story
 
     const self = this;
-
-    console.log('About to do the GET request for the url\n', `${fullPath}\n`);
 
     fetch(fullPath, {
       'method': 'GET',
       'Content-Type': 'application/json'
     })
-      .then((resp) => { console.log('resp', resp); return resp.json(); }) // Transform the data into json
+      .then((resp) => { return resp.json(); }) // Transform the data into json
       .then((res) => {
-        console.log('data', res);
         self.setState(res); // set state with the data
       })
       .catch((e) => {
         console.log('e', e);
       });
-
-    // do an async GET request with the path to get the data and set the state with it
-    // fetch(`https://hrsf84-mvp-server.herokuapp.com/${path}`, { 'mode': 'no-cors' })
-    //   .then(function (response) {
-
-    //     console.log('In the .then for the fetch request');
-
-    //     if (response.status !== 200) {
-    //       console.log('Looks like there was a problem. Status Code: ' + response.status);
-    //       return;
-    //     }
-
-    //     console.log('about to check the response...');
-
-    //     // Examine the data in the response
-    //     response.json()
-    //       .then(function (data) {
-    //         console.log('here\'s the data');
-    //         console.log(data);
-    //         console.log('about to set the state...');
-    //         self.setState(data);
-    //         console.log('end of fetch request');
-    //       });
-    //   })
-    //   .catch(function (err) {
-    //     console.log('Fetch Error :-S', err);
-    //   });
-
   }
-
-  //   {
-  //   "_id": "5a064105fc55700c87b57677",
-  //   "title": "The Second Story",
-  //   "story": "Here is another story. It's even shorter than the first one.",
-  //   "storyId": "5a064105fc55700c87b57676",
-  //   "infoLine": "Fri Nov 10 2017 16:15:01 GMT-0800 (PST)  |  Comments: 0",
-  //   "__v": 0,
-  //   "comments": [],
-  //   "images": [],
-  //   "date": "Fri Nov 10 2017 16:15:01 GMT-0800 (PST)"
-  // }
 
   render() {
     // when figuring out the styling for this render, factor out the non-story reliant portions
--- src/index.js
+++ src/index.js
@@ -4,96 +4,18 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import {
   BrowserRouter as Router,
-  Route,
-  Link
+  Route
 } from 'react-router-dom';
 
 import Story from './Story.jsx';
 
-const sampleStory = {
-  "_id": "5a064105fc55700c87b57677",
-  "title": "The Second Story",
-  "story": "Here is another story. It's even shorter than the first one.",
-  "storyId": "5a064105fc55700c87b57676",
-  "infoLine": "Fri Nov 10 2017 16:15:01 GMT-0800 (PST)  |  Comments: 0",
-  "__v": 0,
-  "comments": [],
-  "images": [],
-  "date": "Fri Nov 10 2017 16:15:01 GMT-0800 (PST)"
-};
-
 const BasicExample = () => (
   <Router>
     <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/topics">Topics</Link></li>
-        <li><Link to='/story'>Story</Link></li>
-        <li><Link to='/The Second Story'>The Second Story</Link></li>
-      </ul>
-
-      <hr />
-
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
-      <Route path='/story' component={Story} story={sampleStory} />
-      {/* <Route path='/The Second Story' component={Story} /> */}
       {<Route path='*' component={Story} />}
-
     </div>
-
   </Router>
 );
-
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-);
-
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>
-          Rendering with React
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>
-          Components
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>
-          Props v. State
-        </Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.url}/:topicId`} component={Topic} />
-    <Route exact path={match.url} render={() => (
-      <h3>Please select a topic.</h3>
-    )} />
-  </div>
-)
-
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-)
 
 ReactDOM.render(<BasicExample />, document.getElementById('root'));
 registerServiceWorker();
