0d7575e936bf3635806f33395cab98d378890d89
Daniel Roberts <BeardOfDan@gmail.com>
2017-11-11 16:28:29 -0800

    Make navbar further comply with react syntax

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -4,28 +4,25 @@ export default class NavBar extends React.Component {
   constructor(props) {
     super(props);
     this.path = this.props.location.pathname;
-    this.fullPath = `https://hrsf84-mvp-server.herokuapp.com${this.path}`;
+    this.fullPath = `https://hrsf84-mvp-client.herokuapp.com${this.path}`;
   }
 
   render() {
     return (
-      <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
+      <nav className="navbar navbar-default navbar-fixed-top">
+        <div className="container">
           <p>This is the navbar</p>
           <button type="button" className="btn btn-primary">Primary</button>
 
-          <ul class="nav nav-pills">
-            <li class="nav-item">
-              <a class="nav-link active" href="{this.fullPath}">Active</a>
+          <ul className="nav nav-pills">
+            <li className="nav-item">
+              <a className="nav-link active" href={this.fullPath} >Active</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{this.fullPath}">Link</a>
+            <li className="nav-item">
+              <a className="nav-link" href={this.fullPath} >Link</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{this.fullPath}">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="{this.fullPath}">Disabled</a>
+            <li className="nav-item">
+              <a className="nav-link" href={this.fullPath} >Link</a>
             </li>
           </ul>
         </div>
