95102c0d2ae4f73a605759832f3c3fcdf07fce2f
Daniel Roberts <BeardOfDan@gmail.com>
2017-11-11 16:44:03 -0800

    Improved NavBar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -3,26 +3,24 @@ import React from 'react';
 export default class NavBar extends React.Component {
   constructor(props) {
     super(props);
-    this.path = this.props.location.pathname;
-    this.fullPath = `https://hrsf84-mvp-client.herokuapp.com${this.path}`;
+    this.currentPath = this.props.location.pathname;
+    this.homePath = `https://hrsf84-mvp-client.herokuapp.com/`;
+    this.fullPath = this.homePath + this.currentPath;
   }
 
   render() {
     return (
       <nav className="navbar navbar-default navbar-fixed-top">
         <div className="container">
-          <p>This is the navbar</p>
-          <button type="button" className="btn btn-primary">Primary</button>
-
           <ul className="nav nav-pills">
             <li className="nav-item">
-              <a className="nav-link active" href={this.fullPath} >Active</a>
+              <a className="nav-link active" href={this.homePath} >Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href={this.fullPath} >Link</a>
+              <a className="nav-link" href={`${this.homePath}The First Story`} >First Story</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href={this.fullPath} >Link</a>
+              <a className="nav-link" href={`${this.homePath}The Second Story`} >The Second Story</a>
             </li>
           </ul>
         </div>
